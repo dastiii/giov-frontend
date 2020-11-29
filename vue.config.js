f9e23539e591b677b5afea6be7f864fb63b637e8
Tobias Schwarz <dastiii@schwarz.id>
2020-11-29 04:30:46 +0100

    wip

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,17 @@ module.exports = {
       args[0].title = "Dein Lieblingsrestaurant.";
       return args;
     });
+  },
+  pwa: {
+    name: "Dein Lieblingsrestaurant.",
+    themeColor: "#499161",
+    msTileColor: "#499161",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: {
+      favicon32: 'img/icons/favicon.png',
+      appleTouchIcon: 'img/icons/favicon-152.png',
+      msTileImage: 'img/icons/favicon-144.png'
+    }
   }
 };
